d256dd7a8c7cb1afbe829cc46d006272d591fa47
Ganeshbhargav <ganeshbhargav619@gmail.com>
2026-02-28 10:34:59 +0530

    feat: Build Custom Range Counter

---
+++ Custom Range Counter/script.js
@@ -0,0 +1,30 @@
+let fromUserInputEl = document.getElementById('fromUserInput');
+let toUserInputEl = document.getElementById('toUserInput');
+let startBtnEl = document.getElementById('startBtn');
+let counterTextEl = document.getElementById('counterText');
+
+function displayNumbers(fromCount,toCount){
+    let currentCount = fromCount;
+    counterTextEl.textContent = currentCount;
+    
+    let timerId = setInterval(()=>{
+        if (currentCount<toCount){
+            currentCount+=1;
+            counterTextEl.textContent = currentCount;
+        }else{
+            clearInterval(timerId);
+        }
+    },1000);
+}
+
+startBtnEl.onclick = ()=>{
+    let fromVal = fromUserInputEl.value;
+    let toVal = toUserInputEl.value ;
+    
+    if (fromVal !=="" && toVal !==""){
+        let fromValInteger = parseInt(fromVal);
+        let toValInteger = parseInt(toVal);
+        
+        displayNumbers(fromValInteger,toValInteger);
+    }
+}
